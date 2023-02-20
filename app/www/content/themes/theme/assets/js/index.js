5a6ff39c3dfb24ca1bab4e87b496a212696967f3
Mikolaj Popielarz <mikolaj.popielarz@gmail.com>
2023-02-20 15:27:47 +0100

    Initial commit

---
+++ app/www/content/themes/theme/assets/js/index.js
@@ -0,0 +1,2 @@
+console.log('test');
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJpbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygndGVzdCcpOyJdLCJmaWxlIjoiaW5kZXguanMifQ==
---
+++ deploy.php
@@ -0,0 +1,100 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+
+set( 'application', 'wptest' );
+set( 'repository', 'git@github.com:mklasen/_repo_.git' );
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set( 'git_tty', true );
+
+// Shared files/dirs between deploys
+set( 'shared_files', array() );
+set( 'shared_dirs', array() );
+set( 'writable_dirs', array() );
+
+set( 'composer_options', 'install --optimize-autoloader' );
+
+host( 'prod' )
+	->stage( 'prod' )
+	->set( 'branch', 'master' )
+	->hostname( 'bijnoabers' )
+	->set( 'deploy_path', '/home/master/sites/__site__/deploy' )
+	->set( 'keep_releases', 5 );
+
+// Symlinks
+task(
+	'deploy:wp_symlinks',
+	function () {
+		writeln( ' - Symlink media directories' );
+		run( 'ln -nfs {{deploy_path}}/shared/uploads {{release_path}}/app/www/content/uploads' );
+
+		writeln( ' - Copy wp-config.php' );
+		run( '[ -f {{deploy_path}}/shared/wp-config.php ] && cp {{deploy_path}}/shared/wp-config.php {{release_path}}/app/www/wp-config.php || cp {{release_path}}/app/www/wp-config.php {{deploy_path}}/shared/wp-config.php' );
+
+		writeln( ' - Environment symlink' );
+		run( 'ln -nfs {{deploy_path}}/shared/.env {{release_path}}/.env' );
+		
+		writeln( ' - htaccess symlink' );
+		run( 'ln -nfs {{deploy_path}}/shared/.htaccess {{release_path}}/app/www/.htaccess' );
+	}
+);
+
+// Robots.txt - we want this on all environments except for production
+task(
+	'deploy:robots_txt',
+	function () {
+		$stage = null;
+		if ( input()->hasArgument( 'stage' ) ) {
+			$stage = input()->getArgument( 'stage' );
+		}
+		if ( 'prod' === $stage ) {
+			$robots_txt = '';
+		} else {
+			$robots_txt = implode(
+				"\n",
+				array(
+					'# This is a staging site. Do not index.',
+					'User-agent: *',
+					'Disallow: /',
+				)
+			);
+		}
+		writeln( 'Uploading robots.txt' );
+		run( "echo '$robots_txt ' > {{release_path}}/app/www/robots.txt" );
+	}
+);
+
+task(
+	'deploy:clear_cache',
+	function() {
+		// run( "touch ~/wordpress/php-fpm.service" );
+	}
+);
+
+// Tasks that run on deployment
+task(
+	'deploy',
+	array(
+		'deploy:info',
+		'deploy:prepare',
+		'deploy:lock',
+		'deploy:release',
+		'deploy:update_code',
+		'deploy:shared',
+		'deploy:writable',
+		'deploy:vendors',
+		'deploy:wp_symlinks',
+		'deploy:robots_txt',
+		'deploy:clear_paths',
+		'deploy:symlink',
+		'deploy:clear_cache',
+		'deploy:unlock',
+		'cleanup',
+		'success',
+	)
+);
+// [Optional] If deploy fails automatically unlock.
+after( 'deploy:failed', 'deploy:unlock' );
